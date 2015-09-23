0dae9df0563073c4ceb456917eefa3970dd1cc64
shaozhaizj1125 <271710227@qq.com>
2015-09-23 17:56:50 +0800

    change2

--- wonderful/test/test19.cpp
+++ wonderful/test/test19.cpp
@@ -3,10 +3,15 @@
 int main()
 {
 	using namespace std;
+
 cout<<"how beautiful"<<endl;
 cout<<"shanghai university!"<<endl;
 	cout << "hello world!"<<endl;
 int count=0;
-char a[40];
+char a[230];
+for(i=0;i<80;i++)
+{
+cout<<endl;
+}
 	return 0;
 }
